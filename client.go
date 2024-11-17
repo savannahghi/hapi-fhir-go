d0b18e0066f4ef23112904af97b1256cbbe6a7dd
Salaton <nairouasalaton@gmail.com>
2024-11-17 20:54:29 +0300

    feat: setup hapi fhir go client (#1)

---
+++ client.go
@@ -0,0 +1,53 @@
+package hapifhirgo
+
+import (
+	"errors"
+	"net/http"
+	"os"
+	"time"
+)
+
+const (
+	defaultTimeout = 10 * time.Second
+)
+
+type Client struct {
+	baseURL string
+
+	HTTP *http.Client
+}
+
+// ClientOption allows customization of the client.
+type ClientOption func(c *Client)
+
+func WithTimeout(t time.Duration) func(c *Client) {
+	return func(c *Client) {
+		c.HTTP.Timeout = t
+	}
+}
+
+// NewClientFromEnvVars creates a new client where the needed fields are
+// retrieved from the environment variables.
+func NewClientFromEnvVars() (*Client, error) {
+	return NewClient(os.Getenv("HAPI_FHIR_BASE_URL"))
+}
+
+// NewClient creates a new hapi fhir api client.
+func NewClient(baseURL string, options ...ClientOption) (*Client, error) {
+	if baseURL == "" {
+		return nil, errors.New("baseURL is empty")
+	}
+
+	client := &Client{
+		HTTP: &http.Client{
+			Timeout: defaultTimeout,
+		},
+		baseURL: baseURL,
+	}
+
+	for _, opt := range options {
+		opt(client)
+	}
+
+	return client, nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/savannahghi/hapi-fhir-go
+
+go 1.22.0
