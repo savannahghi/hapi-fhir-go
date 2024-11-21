d88dcbf8591750b063f78f7e0723a922c95e169a
Salaton <nairouasalaton@gmail.com>
2024-11-21 15:21:21 +0300

    feat: add patient methods

--- patient.go
+++ patient.go
@@ -31,3 +31,35 @@ func (c *Client) CreateFHIRPatient(ctx context.Context, input *models.FHIRPatien
 
 	return output, nil
 }
+
+// GetFHIRPatient retrieves instances of FHIRPatient by ID.
+func (c *Client) GetFHIRPatient(ctx context.Context, id string) (*models.PatientPayload, error) {
+	resource := &models.FHIRPatient{}
+
+	err := c.getFHIRResource(ctx, patientResourceType, id, resource)
+	if err != nil {
+		return nil, fmt.Errorf("unable to get %s with ID %s, err: %w", patientResourceType, id, err)
+	}
+
+	return &models.PatientPayload{PatientRecord: resource}, nil
+}
+
+// GetFHIRPatientEverything is used to retrieve all patient related information.
+func (c *Client) GetFHIRPatientAllData(ctx context.Context, id string, params map[string]interface{}) (*models.Bundle, error) {
+	response, err := c.getPatientEverything(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
+
+// SearchFHIRPatient searches for a FHIR patient.
+func (c *Client) SearchFHIRPatient(ctx context.Context, searchParams map[string]interface{}) (*models.Bundle, error) {
+	response, err := c.searchFHIRResource(ctx, patientResourceType, searchParams)
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
