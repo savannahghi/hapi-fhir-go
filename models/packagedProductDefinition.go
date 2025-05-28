e3ecec3a1e0ce54e507fbaa3b8e56864901ca7f5
Salaton <nairouasalaton@gmail.com>
2025-05-28 11:30:55 +0300

    feat: add packaged product definition model (#45)

---
+++ models/packagedProductDefinition.go
@@ -0,0 +1,119 @@
+package models
+
+import "encoding/json"
+
+// PackagedProductDefinition is documented here http://hl7.org/fhir/StructureDefinition/PackagedProductDefinition
+type PackagedProductDefinition struct {
+	ID                    *string                                        `json:"id,omitempty"`
+	Meta                  *Meta                                          `json:"meta,omitempty"`
+	ImplicitRules         *string                                        `json:"implicitRules,omitempty"`
+	Language              *string                                        `json:"language,omitempty"`
+	Text                  *Narrative                                     `json:"text,omitempty"`
+	Extension             []Extension                                    `json:"extension,omitempty"`
+	ModifierExtension     []Extension                                    `json:"modifierExtension,omitempty"`
+	Identifier            []Identifier                                   `json:"identifier,omitempty"`
+	Name                  *string                                        `json:"name,omitempty"`
+	Type                  *CodeableConcept                               `json:"type,omitempty"`
+	PackageFor            []Reference                                    `json:"packageFor,omitempty"`
+	Status                *CodeableConcept                               `json:"status,omitempty"`
+	StatusDate            *string                                        `json:"statusDate,omitempty"`
+	ContainedItemQuantity []Quantity                                     `json:"containedItemQuantity,omitempty"`
+	Description           *string                                        `json:"description,omitempty"`
+	LegalStatusOfSupply   []PackagedProductDefinitionLegalStatusOfSupply `json:"legalStatusOfSupply,omitempty"`
+	MarketingStatus       []MarketingStatus                              `json:"marketingStatus,omitempty"`
+	CopackagedIndicator   *bool                                          `json:"copackagedIndicator,omitempty"`
+	Manufacturer          []Reference                                    `json:"manufacturer,omitempty"`
+	AttachedDocument      []Reference                                    `json:"attachedDocument,omitempty"`
+	Packaging             *PackagedProductDefinitionPackaging            `json:"packaging,omitempty"`
+	Characteristic        []PackagedProductDefinitionPackagingProperty   `json:"characteristic,omitempty"`
+}
+
+type PackagedProductDefinitionLegalStatusOfSupply struct {
+	ID                *string          `json:"id,omitempty"`
+	Extension         []Extension      `json:"extension,omitempty"`
+	ModifierExtension []Extension      `json:"modifierExtension,omitempty"`
+	Code              *CodeableConcept `json:"code,omitempty"`
+	Jurisdiction      *CodeableConcept `json:"jurisdiction,omitempty"`
+}
+
+type PackagedProductDefinitionPackaging struct {
+	ID                *string                                           `json:"id,omitempty"`
+	Extension         []Extension                                       `json:"extension,omitempty"`
+	ModifierExtension []Extension                                       `json:"modifierExtension,omitempty"`
+	Identifier        []Identifier                                      `json:"identifier,omitempty"`
+	Type              *CodeableConcept                                  `json:"type,omitempty"`
+	ComponentPart     *bool                                             `json:"componentPart,omitempty"`
+	Quantity          *int                                              `json:"quantity,omitempty"`
+	Material          []CodeableConcept                                 `json:"material,omitempty"`
+	AlternateMaterial []CodeableConcept                                 `json:"alternateMaterial,omitempty"`
+	ShelfLifeStorage  []ProductShelfLife                                `json:"shelfLifeStorage,omitempty"`
+	Manufacturer      []Reference                                       `json:"manufacturer,omitempty"`
+	Property          []PackagedProductDefinitionPackagingProperty      `json:"property,omitempty"`
+	ContainedItem     []PackagedProductDefinitionPackagingContainedItem `json:"containedItem,omitempty"`
+	Packaging         []PackagedProductDefinitionPackaging              `json:"packaging,omitempty"`
+}
+
+type PackagedProductDefinitionPackagingProperty struct {
+	ID                   *string          `json:"id,omitempty"`
+	Extension            []Extension      `json:"extension,omitempty"`
+	ModifierExtension    []Extension      `json:"modifierExtension,omitempty"`
+	Type                 CodeableConcept  `json:"type"`
+	ValueCodeableConcept *CodeableConcept `json:"valueCodeableConcept,omitempty"`
+	ValueQuantity        *Quantity        `json:"valueQuantity,omitempty"`
+	ValueDate            *string          `json:"valueDate,omitempty"`
+	ValueBoolean         *bool            `json:"valueBoolean,omitempty"`
+	ValueAttachment      *Attachment      `json:"valueAttachment,omitempty"`
+}
+
+type PackagedProductDefinitionPackagingContainedItem struct {
+	ID                *string           `json:"id,omitempty"`
+	Extension         []Extension       `json:"extension,omitempty"`
+	ModifierExtension []Extension       `json:"modifierExtension,omitempty"`
+	Item              CodeableReference `json:"item"`
+	Amount            *Quantity         `json:"amount,omitempty"`
+}
+
+type OtherPackagedProductDefinition PackagedProductDefinition
+
+// MarshalJSON marshals the given PackagedProductDefinition as JSON into a byte slice.
+func (r PackagedProductDefinition) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		OtherPackagedProductDefinition
+		ResourceType string `json:"resourceType"`
+	}{
+		OtherPackagedProductDefinition: OtherPackagedProductDefinition(r),
+		ResourceType:                   "PackagedProductDefinition",
+	})
+}
+
+// UnmarshalPackagedProductDefinition unmarshals a PackagedProductDefinition.
+func UnmarshalPackagedProductDefinition(b []byte) (PackagedProductDefinition, error) {
+	var packagedProductDefinition PackagedProductDefinition
+	if err := json.Unmarshal(b, &packagedProductDefinition); err != nil {
+		return packagedProductDefinition, err
+	}
+
+	return packagedProductDefinition, nil
+}
+
+type ProductShelfLife struct {
+	ID                           *string           `json:"id,omitempty"`
+	Extension                    []Extension       `json:"extension,omitempty"`
+	ModifierExtension            []Extension       `json:"modifierExtension,omitempty"`
+	Type                         *CodeableConcept  `json:"type,omitempty"`
+	PeriodDuration               *Duration         `json:"periodDuration,omitempty"`
+	PeriodString                 *string           `json:"periodString,omitempty"`
+	SpecialPrecautionsForStorage []CodeableConcept `json:"specialPrecautionsForStorage,omitempty"`
+}
+
+// MarketingStatus is documented here http://hl7.org/fhir/StructureDefinition/MarketingStatus
+type MarketingStatus struct {
+	ID                *string          `json:"id,omitempty"`
+	Extension         []Extension      `json:"extension,omitempty"`
+	ModifierExtension []Extension      `json:"modifierExtension,omitempty"`
+	Country           *CodeableConcept `json:"country,omitempty"`
+	Jurisdiction      *CodeableConcept `json:"jurisdiction,omitempty"`
+	Status            CodeableConcept  `json:"status"`
+	DateRange         *Period          `json:"dateRange,omitempty"`
+	RestoreDate       *string          `json:"restoreDate,omitempty"`
+}
