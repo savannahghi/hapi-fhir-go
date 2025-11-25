a1f1039d0c367609d99c4e709c5a5a6bcf259c2b
Kathurima <41376826+KathurimaKimathi@users.noreply.github.com>
2025-11-25 16:59:48 +0300

    refactor: validate resource before creation (#80)
    - This will eliminate the need to call validate method on the client. Just making it a little strictier.
    - If no resource structure definition is found, default will be used
    
    Signed-off-by: Kathurima Kimathi <kathurimakimathi415@gmail.com>

--- operations.go
+++ operations.go
@@ -23,10 +23,15 @@ import (
 // CreateFHIRResource creates a FHIR resource
 // POST [base]/[type].
 func (c *Client) CreateFHIRResource(ctx context.Context, resourceType string, payload map[string]interface{}, resource interface{}) error {
+	err := c.ValidateResource(ctx, resourceType, payload)
+	if err != nil {
+		return err
+	}
+
 	payload["resourceType"] = resourceType
 	payload["language"] = "EN"
 
-	err := c.makeRequest(ctx, http.MethodPost, resourceType, nil, payload, resource, false)
+	err = c.makeRequest(ctx, http.MethodPost, resourceType, nil, payload, resource, false)
 	if err != nil {
 		return err
 	}
